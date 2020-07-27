34252cd6120e42d50d626815287cdb045220d9b9
Sergey Okhotnikov <ognerezov@gmail.com>
2020-07-27 10:15:15 +0300

    f

---
+++ Everything/View/Viewers/QuotationsView.swift
@@ -0,0 +1,71 @@
+//
+//  QuotationView.swift
+//  Everything
+//
+//  Created by Sergey Okhotnikov on 26.07.2020.
+//  Copyright © 2020 Sergey Okhotnikov. All rights reserved.
+//
+
+import SwiftUI
+
+struct QuotationsView: View {
+    @State var current : Int = 0
+    var chapters : [Chapter]
+    
+    @ViewBuilder
+    var body: some View {
+        if chapters.count > 0 {
+        VStack{
+            HStack{
+                Button(action: next) {
+                    HStack{
+                        Image(systemName:"chevron.left")
+                        Text("Back")
+                    }
+                }
+                .disabled(current == 0)
+                Spacer()
+                Text("Цитаты")
+                Spacer()
+                Button(action: next) {
+                    HStack{
+                        Text("Next")
+                        Image(systemName:"chevron.right")
+                    }
+                }
+            }
+            .padding()
+            ChapterViewer(chapter: chapters[current])
+        }
+        } else{
+            Image(systemName: "arrow.2.circlepath.circle")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 120, height: 120)
+            .padding()
+        }
+    }
+    
+    func next(){
+        current += 1
+        if current == chapters.count{
+            current = 0
+        }
+    }
+    
+    func prev(){
+        current -= 1
+        if current < 0{
+            current = chapters.count - 1
+        }
+    }
+}
+
+struct QuotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuotationsView(chapters: [
+            Chapter(number: 1, type:.chapter , level: 1, records: [
+            ])
+        ])
+    }
+}
