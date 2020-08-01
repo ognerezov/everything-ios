c663989ff347bfdbe317acd928d9466ba8b49405
Sergey Okhotnikov <ognerezov@gmail.com>
2020-08-01 11:37:46 +0300

    change background depending on record type

--- Everything/View/Viewers/RecordViewer.swift
+++ Everything/View/Viewers/RecordViewer.swift
@@ -18,18 +18,28 @@ struct RecordViewer: View  {
         label.sizeToFit()
         return
             ZStack{
-            RoundedRectangle(cornerRadius: 6)
-                .fill(Color(#colorLiteral(red: 0.4620226622, green: 0.8382837176, blue: 1, alpha: 1)))
-            ProxyView(view: LabelFactory.label(content: self.record.spans, width: width))
-            .scaledToFit()
+//                RoundedRectangle(cornerRadius: 6)
+//                    .fill(Color(#colorLiteral(red: 0.4620226622, green: 0.8382837176, blue: 1, alpha: 1)))
+                textBackground
+                ProxyView(view: LabelFactory.label(content: self.record.spans, width: width))
+                .scaledToFit()
             }
-//            .padding(.bottom)
+    }
+    
+    @ViewBuilder
+    var textBackground: some View {
+        if(record.type == .quotation || record.type == .poem){
+            RoundedRectangle(cornerRadius: 6).fill(Color(#colorLiteral(red: 0.4620226622, green: 0.8382837176, blue: 1, alpha: 1)))
+        }
+        else{
+            EmptyView()
+        }
 
     }
 }
 
 struct RecordViewer_Previews: PreviewProvider {
     static var previews: some View {
-        RecordViewer(record: Record(number: 1, type: .chapter, spans: []), width: 300)
+        RecordViewer(record: Record(number: 1, type: .quotation, spans: []), width: 300)
     }
 }
